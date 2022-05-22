b152faeb94d93ba5d00917dd9e1ea68f95f9a6b0
Ewooral <boahen.cos@gmail.com>
2022-05-22 13:24:55 +0000

    JPMorgan software engineering Task 2

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import subprocess
 import time
 import signal
 
-os.chdir(os.getcwd()+'/jpm_module_1')
+os.chdir(os.getcwd()+'/jpm_module_1') 
 
 process = subprocess.Popen(['python', 'server3.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
 
@@ -28,7 +28,7 @@ os.killpg(os.getpgid(process.pid), signal.SIGTERM)
 # IF YOU WANT TO DO IT THEN UNCOMMENT THE CODE BELOW
 # Comments are anything that's preceded with '#'
 # TO UNCOMMENT JUST REMOVE THE '#'
-
+ 
 #print("UNIT TEST RESULTS BELOW...")
 #process2 = subprocess.Popen(['python', #'client_test.py'], cwd=os.getcwd(), #preexec_fn=os.setsid)
 #process2.wait()
