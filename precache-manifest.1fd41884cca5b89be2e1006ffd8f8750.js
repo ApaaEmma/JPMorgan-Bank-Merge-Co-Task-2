dec60155b26e84db88cc1636370b08c05a0bec49
Ewooral <97854982+ApaaEmma@users.noreply.github.com>
2022-05-22 17:28:02 +0000

    Updates

---
+++ precache-manifest.1fd41884cca5b89be2e1006ffd8f8750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a62674b88f71aca03825",
+    "url": "/JPMorgan-Bank-Merge-Co-Task-2/static/css/main.0f61ebe5.chunk.css"
+  },
+  {
+    "revision": "a62674b88f71aca03825",
+    "url": "/JPMorgan-Bank-Merge-Co-Task-2/static/js/main.a62674b8.chunk.js"
+  },
+  {
+    "revision": "3a7246c2cf5e4d8ac7b6",
+    "url": "/JPMorgan-Bank-Merge-Co-Task-2/static/css/1.e00836f2.chunk.css"
+  },
+  {
+    "revision": "3a7246c2cf5e4d8ac7b6",
+    "url": "/JPMorgan-Bank-Merge-Co-Task-2/static/js/1.3a7246c2.chunk.js"
+  },
+  {
+    "revision": "e1d026350bfbca2ef3d3",
+    "url": "/JPMorgan-Bank-Merge-Co-Task-2/static/js/runtime~main.e1d02635.js"
+  },
+  {
+    "revision": "f2c2e614fbf2e29ec31418fac2add1aa",
+    "url": "/JPMorgan-Bank-Merge-Co-Task-2/index.html"
+  }
+];
